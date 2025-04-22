9625e625b13b1b9e8d0509fffaa4fddd68f14daa
Alessandro Assis <aledev21@gmail.com>
2025-04-22 00:47:16 -0300

    style: update comments and improve formatting in keyboard.c

--- src/events/keyboard.c
+++ src/events/keyboard.c
@@ -6,24 +6,25 @@
 /*   By: alessandro <alessandro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 20:06:50 by alessandro        #+#    #+#             */
-/*   Updated: 2025/04/19 22:06:48 by alessandro       ###   ########.fr       */
+/*   Updated: 2025/04/22 00:36:18 by alessandro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
 int handle_key(int key, t_data *data) {
-    if (key == 53)
+    if (key == 53) // ESC
         close_window(data);
-    else if (key == 123)
-        data->move_x -= 10 / data->zoom;
-    else if (key == 124)
-        data->move_x += 10 / data->zoom;
-    else if (key == 126)
-        data->move_y -= 10 / data->zoom;
-    else if (key == 125)
-        data->move_y += 10 / data->zoom;
-    
+    else if (key == 123) // seta esquerda
+        data->move_x -= 10.0 / data->zoom;
+    else if (key == 124) // seta direita
+        data->move_x += 10.0 / data->zoom;
+    else if (key == 126) // seta cima
+        data->move_y -= 10.0 / data->zoom;
+    else if (key == 125) // seta baixo
+        data->move_y += 10.0 / data->zoom;
+
+    update_fractal_limits(data);  // <== ESSENCIAL
     render(data);
-    return (0);
+    return 0;
 }
