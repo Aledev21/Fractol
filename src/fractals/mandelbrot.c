4f9b1d49fa474d16629d20ca33c1cb3c7cd8ffd7
Alessandro Assis <aledev21@gmail.com>
2025-04-22 00:47:36 -0300

    style: improve code formatting and consistency in mandelbrot.c

--- src/fractals/mandelbrot.c
+++ src/fractals/mandelbrot.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandelbrot.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alessandro <alessandro@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 23:20:45 by alessandro        #+#    #+#             */
+/*   Updated: 2025/04/22 00:46:25 by alessandro       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/fractol.h"
 
 void mandelbrot(t_data *data, int x, int y)
@@ -8,13 +20,20 @@ void mandelbrot(t_data *data, int x, int y)
     double zy = 0;
     int iter = 0;
 
-    while (zx*zx + zy*zy < 4 && iter < data->max_iter)
+    while (zx * zx + zy * zy < 4 && iter < data->max_iter)
     {
-        double tmp = zx*zx - zy*zy + cx;
+        double tmp = zx * zx - zy * zy + cx;
         zy = 2 * zx * zy + cy;
         zx = tmp;
         iter++;
     }
-    
+
+    if (iter < data->max_iter)
+    {
+        double log_zn = log(zx * zx + zy * zy) / 2;
+        double nu = log(log_zn / log(2)) / log(2);
+        iter = iter + 1 - nu;
+    }
+
     put_pixel(data, x, y, get_color(iter, data->max_iter));
 }
