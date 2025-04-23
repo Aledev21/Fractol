293f4631baf4296bf40c9faaa20e2483a0cde90a
Alessandro Assis <aledev21@gmail.com>
2025-04-23 03:10:49 -0300

    style: update key codes for consistency and improve comments in keyboard.c

--- src/events/keyboard.c
+++ src/events/keyboard.c
@@ -6,25 +6,21 @@
 /*   By: alessandro <alessandro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 20:06:50 by alessandro        #+#    #+#             */
-/*   Updated: 2025/04/22 00:36:18 by alessandro       ###   ########.fr       */
+/*   Updated: 2025/04/23 03:09:53 by alessandro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
 int handle_key(int key, t_data *data) {
-    if (key == 53) // ESC
+    if (key == 65307)  // ESC - geralmente correto para X11
         close_window(data);
-    else if (key == 123) // seta esquerda
-        data->move_x -= 10.0 / data->zoom;
-    else if (key == 124) // seta direita
-        data->move_x += 10.0 / data->zoom;
-    else if (key == 126) // seta cima
+    else if (key == 65362)  // seta cima
         data->move_y -= 10.0 / data->zoom;
-    else if (key == 125) // seta baixo
+    else if (key == 65364)  // seta baixo
         data->move_y += 10.0 / data->zoom;
 
-    update_fractal_limits(data);  // <== ESSENCIAL
+    update_fractal_limits(data);
     render(data);
     return 0;
 }
