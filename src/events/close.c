b67446e6b04cbe42a3842abdf4fcff2659ab3d53
Alessandro Assis <aledev21@gmail.com>
2025-04-23 03:10:41 -0300

    style: update timestamp in header comment for close.c and improve free_all function for safety

--- src/events/close.c
+++ src/events/close.c
@@ -6,7 +6,7 @@
 /*   By: alessandro <alessandro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 20:57:45 by alessandro        #+#    #+#             */
-/*   Updated: 2025/04/23 02:09:06 by alessandro       ###   ########.fr       */
+/*   Updated: 2025/04/23 02:20:46 by alessandro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,24 @@ int close_window(t_data *data)
 
 void free_all(t_data *data)
 {
-    if (data->img)
+    if (!data)
+        return;
+        
+    if (data->img && data->mlx)
         mlx_destroy_image(data->mlx, data->img);
-    if (data->win)
+    if (data->win && data->mlx)
         mlx_destroy_window(data->mlx, data->win);
 
     #ifdef __linux__
-        mlx_destroy_display(data->mlx);
-        free(data->mlx);
+        if (data->mlx)
+        {
+            mlx_destroy_display(data->mlx);
+            free(data->mlx);
+        }
     #endif
+    
+    // Limpa outros recursos se necessário
+    data->mlx = NULL;
+    data->win = NULL;
+    data->img = NULL;
 }
