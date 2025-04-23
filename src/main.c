c08a2e4bb84318aa93f3286e1dcc0a9153c127ee
Alessandro Assis <aledev21@gmail.com>
2025-04-23 03:10:21 -0300

    style: update timestamp in header comment for main.c and improve code formatting

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: alessandro <alessandro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 18:47:36 by alessandro        #+#    #+#             */
-/*   Updated: 2025/04/21 23:31:07 by alessandro       ###   ########.fr       */
+/*   Updated: 2025/04/23 02:34:06 by alessandro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,15 +64,23 @@ void init_fractal(t_data *data, int argc, char **argv)
 int main(int argc, char **argv)
 {
     t_data data;
-
+    
+    // Inicializa a estrutura com zeros
+    ft_memset(&data, 0, sizeof(t_data));
+    
     init_fractal(&data, argc, argv);
-    init_mlx(&data);
-    mlx_hook(data.win, 2, 1L << 0, handle_key, &data);
-    mlx_mouse_hook(data.win, handle_mouse, &data);
-    mlx_hook(data.win, 17, 0, close_window, &data);
-
+    
+    init_mlx_safe(&data);
+    if (data.mlx == NULL) 
+    {
+        free_all(&data);
+        exit(EXIT_FAILURE);
+    }
+    
     render(&data);
     mlx_loop(data.mlx);
-
+    
+    // Nunca deve chegar aqui se o loop estiver funcionando corretamente
+    free_all(&data);
     return (0);
 }
